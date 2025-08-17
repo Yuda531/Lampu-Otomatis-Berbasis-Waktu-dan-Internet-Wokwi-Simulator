83b8b0d0bb680342d8167b37dde216f197de2687
Yuda531 <yudaagung70@gmail.com>
2025-08-17 11:27:49 +0700

    Adding RTC

--- sketch.ino
+++ sketch.ino
@@ -1,6 +1,8 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "time.h"
+#include <Wire.h>
+#include "RTClib.h"
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
@@ -12,12 +14,17 @@ const int ledPins[] = {2, 4, 5, 18, 19};
 const int ledCount = sizeof(ledPins) / sizeof(ledPins[0]);
 bool lampuHidup = false;
 
+RTC_DS1307 rtc;
+
 void setup() {
   Serial.begin(115200);
+  Wire.begin();
+
   for (int i = 0; i < ledCount; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
 
+  // Hubungkan ke WiFi
   WiFi.begin(ssid, password);
   Serial.print("Menghubungkan ke WiFi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -26,21 +33,49 @@ void setup() {
   }
   Serial.println(" Terhubung!");
 
-  configTime(7 * 3600, 0, "pool.ntp.org", "time.nist.gov"); // UTC+7 untuk WIB
+  // Sinkronisasi waktu ke NTP
+  configTime(7 * 3600, 0, "pool.ntp.org", "time.nist.gov"); // UTC+7 WIB
+
+  if (!rtc.begin()) {
+    Serial.println("RTC tidak ditemukan!");
+    while (1);
+  }
+
+  if (!rtc.isrunning()) {
+    Serial.println("RTC tidak berjalan, set waktu sekarang...");
+    struct tm timeinfo;
+    if (getLocalTime(&timeinfo)) {
+      rtc.adjust(DateTime(1900 + timeinfo.tm_year, timeinfo.tm_mon + 1,
+                          timeinfo.tm_mday, timeinfo.tm_hour,
+                          timeinfo.tm_min, timeinfo.tm_sec));
+    }
+  }
 }
 
 void loop() {
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("Gagal mendapatkan waktu");
-    return;
+
+  int hourNow;
+  int minuteNow;
+  int secondNow;
+
+  if (getLocalTime(&timeinfo)) {
+    // Ambil waktu dari NTP
+    hourNow = timeinfo.tm_hour;
+    minuteNow = timeinfo.tm_min;
+    secondNow = timeinfo.tm_sec;
+  } else {
+    // Jika gagal, pakai RTC
+    DateTime now = rtc.now();
+    hourNow = now.hour();
+    minuteNow = now.minute();
+    secondNow = now.second();
   }
 
-  int hourNow = timeinfo.tm_hour;
-  Serial.printf("Jam sekarang: %02d:%02d:%02d\n", hourNow, timeinfo.tm_min, timeinfo.tm_sec);
+  Serial.printf("Jam sekarang: %02d:%02d:%02d\n", hourNow, minuteNow, secondNow);
 
-
-  if (hourNow >= 9 || hourNow < 6) {
+  // Logika Lampu: ON dari jam 11 siang sampai jam 6 pagi
+  if (hourNow >= 11 || hourNow < 6) {
     for (int i = 0; i < ledCount; i++) {
       digitalWrite(ledPins[i], HIGH);
     }
@@ -53,7 +88,7 @@ void loop() {
   }
 
   kirimStatusKeThingspeak(lampuHidup ? 1 : 0);
-  delay(15000); // Kirim data setiap 15 detik 
+  delay(15000); // kirim setiap 15 detik
   Serial.println(lampuHidup ? "Lampu HIDUP" : "Lampu MATI");
 }
 
@@ -69,7 +104,6 @@ void kirimStatusKeThingspeak(int status) {
     } else {
       Serial.println("Gagal mengirim data");
     }
-
     http.end();
   }
 }
